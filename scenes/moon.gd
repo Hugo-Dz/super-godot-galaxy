ced20c598f96b3ec420e1f079fa32daf7aa5028a
Hugo <duprez.hugo@gmail.com>
2023-10-09 14:36:55 +0200

    feat: planet props

---
+++ scenes/moon.gd
@@ -0,0 +1,15 @@
+extends Sprite3D
+
+@export var view: Node
+@export var sun: DirectionalLight3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if view and sun:
+		var sun_direction = sun.global_transform.basis.z.normalized() 
+		global_transform.origin = view.global_transform.origin + sun_direction * 200
