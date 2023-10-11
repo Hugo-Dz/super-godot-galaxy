236fdf5699ee227c1bb405aa807d51196b273f37
Hugo <duprez.hugo@gmail.com>
2023-10-11 13:13:00 +0200

    feat: polish

--- objects/coin.gd
+++ objects/coin.gd
@@ -21,7 +21,14 @@ func _on_body_entered(body):
 
 func _process(delta):
 	
-	rotate_y(2 * delta) # Rotation
-	position.y += (cos(time * 5) * 1) * delta # Sine movement
+	var rotation_amount = deg_to_rad(90) * delta * 0.25
+	var rotation_quaternion = Quaternion(Vector3(0, 1, 0), rotation_amount)
+	var combined_rotation = quaternion * rotation_quaternion
+	transform.basis = Basis(combined_rotation)
+	
+	var move_distance = (cos(time * 5) * 1) * delta
+	var local_movement = transform.basis.y * move_distance
+
+	global_transform.origin += local_movement
 	
 	time += delta
